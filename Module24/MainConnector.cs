dcc3143f549b8435cc1699f72ba67f71ebe1a420
AreURdy4TheRock <dorkwingdock@gmail.com>
2023-08-02 18:16:29 +0300

    Добавьте файлы проекта.

---
+++ Module24/MainConnector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Module24.Configurations;
+
+namespace Module24
+{
+    public class MainConnector
+    {
+        SqlConnection connection = new SqlConnection(ConnectionString.MsSqlConnection);
+        public async Task<bool> ConnectAsync()
+        {
+            bool result;
+            try
+            {
+                await connection.OpenAsync();
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+        public void DisconnectAsync()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+        public SqlConnection GetConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                return connection;
+            }
+            else
+            {
+                throw new Exception("Подключение уже закрыто!");
+            }
+        }
+    }
+}
